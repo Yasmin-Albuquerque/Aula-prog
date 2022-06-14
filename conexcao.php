80b778f8bbd6a06d3e7deb68febae26badde8605
Yasmin Albuquerque <106176758+Yasmin-Albuquerque@users.noreply.github.com>
2022-06-14 11:19:49 -0500

    Add files via upload

---
+++ conexcao.php
@@ -0,0 +1,15 @@
+<?php
+    $servidor = "localhost";
+    $bdname = "cadastro";
+    $usuario = "root";
+    $senha = "";
+
+    $conexao = mysqli_connect ($servidor, $usuario, $senha, $bdname);
+    
+    if (!$conexao)
+    {
+        die("Problemas com a conexão com o banco de dados. Descrição do problema: " . mysqli_connect_error());
+    }
+
+    echo "teste";
+?>
